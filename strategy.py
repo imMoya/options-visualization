fa0c21afc21f0ad87d10bc38bfaab3c8edd3ef98
Nacho <imoyared@gmail.com>
2021-12-27 23:53:33 +0100

    Descriptive functions

--- strategy.py
+++ strategy.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 from options import options
 
 def inside_array(x_list):
+    """ Return the min value a max value of an intersection of several arrays included in a list
+    :param x_list: list with arrays inside
+    :return min and max value of the intersection of the arrays in the x_list
+    """
     x_min = 0
     x_max = 1e5
     for i in range(len(x_list)):
@@ -11,27 +15,43 @@ def inside_array(x_list):
     return x_min, x_max
 
 def closest(x, y, K):
-     x = np.asarray(x)
-     ind = (np.abs(x - K)).argmin()
-     return y[ind]
+    """Return the y(x closest to K)
+    :param x: array (e.g. associated to x values in a plot)
+    :param y: array (e.g. associated to y values in a plot)  
+    :return y(x closest to K)
+    """
+    x = np.asarray(x)
+    ind = (np.abs(x - K)).argmin()
+    return y[ind]
 
 class opt_inf():
     def __init__(self, K=0, pos='L', n=1):
+        """Include information of options
+        :param K: strike price at expiration date
+        :param pos: type of position in an option ('L' if long, or 'S' if short)
+        :param n: number of options """
         self.K = K
         self.pos = pos 
         self.n = n
 
 class payoff_inf():
     def __init__(self, x, y):
+        """Include two arrays (x, y) in an object
+        :param x: array (e.g. associated to x values in a plot)
+        :param y: array (e.g. associated to y values in a plot)  
+        """
         self.x = x
         self.y = y
 
 class strategy():
     def __init__(self, opt=[]):
+        """Initialize the option class
+        :param opt: option class (refer to opt_inf)
+        """
         self.opt = opt
 
     def call(self):
-        """Return the payoff of a call strategy with strike price K1
+        """Return the payoff of a call strategy with strike price K
         """
         K = self.opt.K
         pos = 1 if self.opt.pos == 'L' else -1
@@ -54,6 +74,11 @@ class strategy():
 
     @staticmethod
     def combine(payoff):
+        """Returns the payoff of different strategies.
+        :param payoff: list of strategies defined (refer to main function)
+        :return x: array containing prices surrounding the strike price of the strategy
+        :return y: array containing the payoff for each price
+        """
         x_list = []
         [x_list.append(p.x) for p in payoff]
         x_min, x_max = inside_array(x_list)
@@ -66,7 +91,13 @@ class strategy():
         return x, y
 
 def main():
-    """The main function returns the strategy defined.
+    """The main function returns the strategy defined to buy/sell different options.
+    The user must define the payoff as a list of different strategies. 
+    Each item in the list has to call the strategy class, defining the option:
+        K (strike price)
+        position ('L'/'S')
+        n (number of options)
+        call() or put()
     """
     payoff = [strategy(opt=opt_inf(K=1, pos='L', n=2)).call(),
               strategy(opt=opt_inf(K=1, pos='L', n=2)).put()]
